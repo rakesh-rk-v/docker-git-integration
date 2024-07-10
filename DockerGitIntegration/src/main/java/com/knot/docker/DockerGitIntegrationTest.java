02f1765357c0d78a5e4061968d8d069e1ee8e17e
Rakesh Kumar <rakeshvendrapati173@gmail.com>
2024-07-10 18:32:51 +0530

    Added the Git Docker Integration Container in to the Git-Hub

---
+++ DockerGitIntegration/src/main/java/com/knot/docker/DockerGitIntegrationTest.java
@@ -0,0 +1,13 @@
+package com.knot.docker;
+
+public class DockerGitIntegrationTest {
+
+	public static void main(String[] args) throws Exception {
+		
+		for(int i = 0 ; i < 100; i++){
+			System.out.println("Hello Rakesh Ping " + i );
+			Thread.sleep(1000);
+		}
+	}
+
+}
